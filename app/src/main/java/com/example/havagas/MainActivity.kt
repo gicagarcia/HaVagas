2a7ddc3783d7187d228e7962d7ca8721cc77e0d1
User <gicagarcia58@gmail.com>
2024-03-05 19:22:32 -0300

    config visibilidade celular onCheck

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.havagas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import com.example.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.mainTb)
+        supportActionBar?.title = getString(R.string.app_name)
+
+        with(amb){
+            celularCb.setOnCheckedChangeListener { buttonView, isChecked ->
+                if(isChecked){
+                    celularLl.visibility = View.VISIBLE
+                }else{
+                    celularLl.visibility = View.GONE
+                }
+
+            }
+        }
     }
 }
